541b96247440725bfab40018170808225351bb68
khushi rai <khushirai4002@gmail.com>
2024-05-16 16:55:06 +0530

    django authentication

--- carts/views.py
+++ carts/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render,redirect
-from store.models import Product,Variation
-from .models import Cart,CartItem
+from django.shortcuts import render, redirect
+from store.models import Product, Variation
+from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
-
-# Create your views here.
 from django.http import HttpResponse
+
 def _cart_id(request):
-    cart = request.session.session_key
-    if not cart:
-        cart = request.session.create()
-    return cart
+    cart_id = request.session.session_key
+    if not cart_id:
+        request.session.create()
+        cart_id = request.session.session_key
+    return cart_id
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     product_variations = []
+
     if request.method == 'POST':
         for item in request.POST:
             key = item
@@ -32,27 +33,28 @@ def add_cart(request, product_id):
     is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
     
     if is_cart_item_exists:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        existing_variations = cart_item.variations.all()
-        
-        if set(product_variations) == set(existing_variations):
-            cart_item.quantity += 1
-            cart_item.save()
-        else:
-            new_cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
-            new_cart_item.variations.set(product_variations)
+        try:
+            cart_item = CartItem.objects.get(product=product, cart=cart)
+            existing_variations = cart_item.variations.all()
+            
+            if set(product_variations) == set(existing_variations):
+                cart_item.quantity += 1
+                cart_item.save()
+            else:
+                new_cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+                new_cart_item.variations.set(product_variations)
+        except CartItem.DoesNotExist:
+            pass
     else:
         new_cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
         new_cart_item.variations.set(product_variations)
     
     return redirect('cart')
 
-def remove_cart_item(request,product_id):
+def remove_cart_item(request, product_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = Product.objects.get(id=product_id)
-
-    cart_item = CartItem.objects.filter(product=product,cart=cart)
-
+    cart_item = CartItem.objects.filter(product=product, cart=cart)
     cart_item.delete()
     return redirect('cart')
     
@@ -64,13 +66,15 @@ def subtract_cart(request, product_id):
     except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=_cart_id(request))
     
-    cart_item, created = CartItem.objects.get_or_create(product=product, cart=cart)
-    if not created:
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
         if cart_item.quantity > 1:
             cart_item.quantity -= 1
             cart_item.save()
         else:
             cart_item.delete()
+    except CartItem.DoesNotExist:
+        pass
     
     return redirect('cart')
 
@@ -81,16 +85,16 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        tax =(2 * total)/100
+        tax = (2 * total) / 100
         grand_total = total + tax 
     except ObjectDoesNotExist:
         pass
     
     context ={
-        'total':total,
-        'quantity':quantity,
-        'cart_items':cart_items,
-        'tax'      :tax,
-        'grand_total':grand_total,
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax if 'tax' in locals() else 0,
+        'grand_total': grand_total if 'grand_total' in locals() else 0,
     }
-    return render(request,'store/cart.html',context)
+    return render(request, 'store/cart.html', context)
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -136,3 +136,16 @@ MEDIA_ROOT =BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",       
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER ='khushirai252@gmail.com'
+EMAIL_HOST_PASSWORD = 'Piyush_2007'
